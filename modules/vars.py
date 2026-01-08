f12e105e439a0dedd15fe9f94bc5a595bb391f04
gamerzh910-gif <gamerzh910@gmail.com>
2026-01-08 09:53:52 +0530

    Update default values for TOTAL_USERS and AUTH_USERS

--- modules/vars.py
+++ modules/vars.py
@@ -11,10 +11,10 @@ OWNER = int(environ.get("OWNER", "8322536400"))
 CREDIT = environ.get("CREDIT", "🤍🌸@leavingproperty🤍🌸")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '7549194607').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8322536400').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '7549194607').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8322536400').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
